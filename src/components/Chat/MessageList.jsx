c03a4b9a507898dfea2b56c78906bf50b42bbd77
Nitin Momaya <nitinmomaya5@gmail.com>
2023-08-15 13:30:05 +0530

    Fixed CSS Issues

--- src/components/Chat/MessageList.jsx
+++ src/components/Chat/MessageList.jsx
@@ -7,9 +7,9 @@ const MessageList = ({ messages }) => {
   ];
   return (
     <div className="bg-white  m-3 gap-2 flex flex-col h-40 overflow-y-scroll overflow-x-hidden justify-self-end">
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 w-60">
         {recieverMessage.map((message) => (
-          <div className="p-2 bg-slate-100 text-sm rounded-md w-64">
+          <div className="p-2 bg-slate-100 text-sm rounded-md w-48">
             <h1>{message}</h1>
           </div>
         ))}
@@ -17,11 +17,11 @@ const MessageList = ({ messages }) => {
       <div className="w-full flex justify-center">
         <h1 className="text-slate-400 text-sm text-center">9:41 pm</h1>
       </div>
-      <div className="w-full  flex flex-col gap-2  ">
+      <div className=" flex flex-col gap-2  ">
         <div className="flex flex-col gap-3 items-end m-2 ">
           {messages.map((message, index) => (
             <p
-              className=" flex flex-col gap-3 p-2 bg-slate-100 text-sm rounded-md w-64 text-right"
+              className=" flex flex-col gap-3 p-2 bg-slate-100 text-sm rounded-md w-48 text-right"
               key={index}
             >
               {message}
